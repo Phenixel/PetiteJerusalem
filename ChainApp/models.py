6cad44e27189de44fe60402798738110abf24820
Phenixel <yonathancardoso@outlook.fr>
2024-03-10 14:02:05 +0100

    (feat) ajout des 3 Models pour le projet

--- ChainApp/models.py
+++ ChainApp/models.py
@@ -1,4 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+
+class Person(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.user.username
 
 
 class Session(models.Model):
@@ -6,6 +14,7 @@ class Session(models.Model):
     description = models.TextField()
     date_limit = models.DateField()
     created_at = models.DateTimeField(auto_now_add=True)
+    person = models.ForeignKey('Person', on_delete=models.SET_NULL, blank=True, null=True)
 
     def __str__(self):
         return self.name
@@ -15,6 +24,7 @@ class Gemara(models.Model):
     name = models.CharField(max_length=255)
     available = models.BooleanField(default=True)
     session = models.ForeignKey('Session', on_delete=models.CASCADE, blank=True, null=True)
+    chosen_by = models.ForeignKey('Person', on_delete=models.SET_NULL, blank=True, null=True)
 
     def __str__(self):
         return self.name
