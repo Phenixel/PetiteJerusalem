b970e3578a06d910467ff58816e0d9afee0598ff
Phenixel <yonathancardoso@outlook.fr>
2024-03-10 22:59:16 +0100

    (fix) PST-11 rendre impossible la création de session pour une date passée

--- ChainApp/views.py
+++ ChainApp/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, redirect
 from django.views import View
 from .models import Session
 from datetime import date
+from django.utils import timezone
+from datetime import datetime
 
 
 class HomeView(View):
@@ -19,14 +21,22 @@ class CreateSessionView(View):
     def post(self, request):
         name = request.POST.get('name')
         description = request.POST.get('description')
-        date_limit = request.POST.get('date_limit')
+        date_limit_str = request.POST.get('date_limit')
+
+        # Convertir la chaîne de caractères en objet datetime.date
+        date_limit = datetime.strptime(date_limit_str, "%Y-%m-%d").date()
+
+        # Vérifier si la date limite est déjà passée
+        if timezone.now().date() > date_limit:
+            error_message = "La date limite doit être une date future."
+            return render(request, 'ChainApp/create_session.html', {'error_message': error_message})
 
         # Créer une nouvelle session
         new_session = Session(name=name, description=description, date_limit=date_limit)
         new_session.save()
 
         # Rediriger vers une page de confirmation ou une autre vue
-        return redirect('home')  # Rediriger vers la page d'accueil après création
+        return redirect('home')
 
 
 class SessionDetailView(View):
