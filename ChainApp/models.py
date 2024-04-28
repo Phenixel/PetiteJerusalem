bf1477bc9a4a4f3af6f207613e99f687c6789ec3
Phenixel <yonathancardoso@outlook.fr>
2024-04-28 15:06:34 +0200

    (feat) PST-16 #in_progress connexion à l'application

--- ChainApp/models.py
+++ ChainApp/models.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import User
 
 class Person(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
 
     def __str__(self):
         return self.user.username
