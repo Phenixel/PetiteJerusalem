7168c5cfc67eac819b21746e07d5d424972be3fc
Nathan_Sroussi <77548288+NathanSr08@users.noreply.github.com>
2024-09-10 23:26:59 +0200

    dev-session_michna

--- ChainApp/views.py
+++ ChainApp/views.py
@@ -1,11 +1,14 @@
 from django.contrib.auth.models import User
 from django.shortcuts import render, redirect, get_object_or_404
 from django.views import View
-from .models import Session, Gemara, Gemarot, Person, Guest
+from .models import Session, Gemara, Gemarot, Person, Guest, Massekhet, Michna, Seder
 from datetime import date
 from django.utils import timezone
 from django.contrib.auth import authenticate, login, logout
 import dateparser
+from django.db.models import Count, Q
+from django.contrib import messages
+
 
 
 class HomeView(View):
@@ -83,6 +86,7 @@ class CreateSessionView(View):
         name = request.POST.get('name')
         description = request.POST.get('description')
         date_limit_str = request.POST.get('date_limit')
+        session_type = request.POST.get('session_type')  # Récupère la valeur du bouton radio
 
         date_limit = dateparser.parse(date_limit_str, date_formats=['%Y-%m-%d']).date()
 
@@ -98,81 +102,149 @@ class CreateSessionView(View):
             name=name,
             description=description,
             date_limit=date_limit,
-            person=person
+            person=person,
+            session_is=True if session_type == "mishna" else False  # Définit le type de session
         )
         new_session.save()
 
         return redirect('home')
 
-
 class SessionDetailView(View):
     def get(self, request, slug):
         session = get_object_or_404(Session, slug=slug)  # Récupérer la session par slug
 
-        gemarot = Gemarot.objects.all()
-        gemarot_list = []
-        # logger.info(f"Users connecté : {request.user}")
-        for gemara in gemarot:
-            # Vérifier si la Gemara est réservée pour cette session par n'importe quel utilisateur
-            gemara_reservation = Gemara.objects.filter(session=session, choose_gemarot=gemara).first()
-            if request.user.is_authenticated:
-                reserved_by_user = gemara_reservation and gemara_reservation.chosen_by == request.user.person
-            else:
-                reserved_by_user = None
+        if session.session_is:
+            sedarim = Seder.objects.prefetch_related('massekhtot').all()
+            masekhtot_list = []
 
-            gemarot_list.append({
-                'gemara': gemara,
-                'reserved': gemara_reservation is not None,
-                'chosen_by_username': (
-                    gemara_reservation.chosen_by.user.username if gemara_reservation and gemara_reservation.chosen_by else
-                    gemara_reservation.chosen_by_guest.name if gemara_reservation and gemara_reservation.chosen_by_guest else None),
-                'reserved_by_user': reserved_by_user,
-            })
+            for seder in sedarim:
+                seder_data = {
+                    'seder': seder,
+                    'massekhtot': []
+                }
 
-        context = {
-            'session': session,
-            'gemarot_list': gemarot_list,
-        }
+                for masekhet in seder.massekhtot.all():
+                    masekhet_reservation = Michna.objects.filter(session=session, choose_michna=masekhet).first()
+                    reserved_by_user = masekhet_reservation and masekhet_reservation.chosen_by == request.user.person if request.user.is_authenticated else None
+
+                    seder_data['massekhtot'].append({
+                        'masekhet': masekhet,
+                        'reserved': masekhet_reservation is not None,
+                        'chosen_by_username': (
+                            masekhet_reservation.chosen_by.user.username if masekhet_reservation and masekhet_reservation.chosen_by else
+                            masekhet_reservation.chosen_by_guest.name if masekhet_reservation and masekhet_reservation.chosen_by_guest else None),
+                        'reserved_by_user': reserved_by_user,
+                    })
+
+                masekhtot_list.append(seder_data)
+
+            context = {
+                'session': session,
+                'seder_list': masekhtot_list,  # Utilisez 'seder_list' pour l'envoyer au template
+            }
+        else:
+            # Si la session est une session de Gemara
+            gemarot = Gemarot.objects.all()
+            gemarot_list = []
+            for gemara in gemarot:
+                gemara_reservation = Gemara.objects.filter(session=session, choose_gemarot=gemara).first()
+                if request.user.is_authenticated:
+                    reserved_by_user = gemara_reservation and gemara_reservation.chosen_by == request.user.person
+                else:
+                    reserved_by_user = None
+
+                gemarot_list.append({
+                    'gemara': gemara,
+                    'reserved': gemara_reservation is not None,
+                    'chosen_by_username': (
+                        gemara_reservation.chosen_by.user.username if gemara_reservation and gemara_reservation.chosen_by else
+                        gemara_reservation.chosen_by_guest.name if gemara_reservation and gemara_reservation.chosen_by_guest else None),
+                    'reserved_by_user': reserved_by_user,
+                })
+
+            context = {
+                'session': session,
+                'gemarot_list': gemarot_list,
+            }
+
         return render(request, 'ChainApp/session_detail.html', context)
 
     def post(self, request, slug):
         session = get_object_or_404(Session, slug=slug)  # Récupérer la session par slug
-        gemara_ids = request.POST.getlist('gemarot')  # Récupère une liste d'IDs des Gemarot sélectionnées
 
-        if request.user.is_authenticated:
-            person = request.user.person
-            # logger.info(f"Profil Person trouvé : {person}")
-            user_reservations = Gemara.objects.filter(session=session, chosen_by=person)
+        if session.session_is:
+            masekhet_ids = request.POST.getlist('masekhtot')  # Récupère une liste d'IDs des Massekhtot sélectionnées
 
-            # Annuler les réservations décochées (seulement si l'utilisateur est connecté)
-            for reservation in user_reservations:
-                if str(reservation.choose_gemarot.id) not in gemara_ids:
-                    reservation.delete()
+            if request.user.is_authenticated:
+                person = request.user.person
+                user_reservations = Michna.objects.filter(session=session, chosen_by=person)
 
-            # Ajouter les nouvelles réservations cochées
-            for gemara_id in gemara_ids:
-                gemarot = get_object_or_404(Gemarot, pk=gemara_id)
-                if not Gemara.objects.filter(session=session, choose_gemarot=gemarot, chosen_by=person).exists():
-                    Gemara.objects.create(
-                        session=session,
-                        choose_gemarot=gemarot,
-                        chosen_by=person,
-                        available=False
-                    )
+                # Annuler les réservations décochées (seulement si l'utilisateur est connecté)
+                for reservation in user_reservations:
+                    if str(reservation.choose_michna.id) not in masekhet_ids:
+                        reservation.delete()
+
+                # Ajouter les nouvelles réservations cochées
+                for masekhet_id in masekhet_ids:
+                    masekhet = get_object_or_404(Massekhet, pk=masekhet_id)
+                    if not Michna.objects.filter(session=session, choose_michna=masekhet, chosen_by=person).exists():
+                        Michna.objects.create(
+                            session=session,
+                            choose_michna=masekhet,
+                            chosen_by=person,
+                            available=False
+                        )
+            else:
+                guest_name = request.POST.get('guest_name')
+                guest_email = request.POST.get('guest_email')
+                guest, created = Guest.objects.get_or_create(email=guest_email, defaults={'name': guest_name})
+
+                # Ajouter les nouvelles réservations cochées
+                for masekhet_id in masekhet_ids:
+                    masekhet = get_object_or_404(Massekhet, pk=masekhet_id)
+                    if not Michna.objects.filter(session=session, choose_michna=masekhet, chosen_by_guest=guest).exists():
+                        Michna.objects.create(
+                            session=session,
+                            choose_michna=masekhet,
+                            chosen_by_guest=guest,
+                            available=False
+                        )
         else:
-            guest_name = request.POST.get('guest_name')
-            guest_email = request.POST.get('guest_email')
-            guest, created = Guest.objects.get_or_create(email=guest_email, defaults={'name': guest_name})
+            gemara_ids = request.POST.getlist('gemarot')  # Récupère une liste d'IDs des Gemarot sélectionnées
 
-            # Ajouter les nouvelles réservations cochées
-            for gemara_id in gemara_ids:
-                gemarot = get_object_or_404(Gemarot, pk=gemara_id)
-                if not Gemara.objects.filter(session=session, choose_gemarot=gemarot, chosen_by_guest=guest).exists():
-                    Gemara.objects.create(
-                        session=session,
-                        choose_gemarot=gemarot,
-                        chosen_by_guest=guest,
-                        available=False
-                    )
+            if request.user.is_authenticated:
+                person = request.user.person
+                user_reservations = Gemara.objects.filter(session=session, chosen_by=person)
+
+                # Annuler les réservations décochées (seulement si l'utilisateur est connecté)
+                for reservation in user_reservations:
+                    if str(reservation.choose_gemarot.id) not in gemara_ids:
+                        reservation.delete()
+
+                # Ajouter les nouvelles réservations cochées
+                for gemara_id in gemara_ids:
+                    gemarot = get_object_or_404(Gemarot, pk=gemara_id)
+                    if not Gemara.objects.filter(session=session, choose_gemarot=gemarot, chosen_by=person).exists():
+                        Gemara.objects.create(
+                            session=session,
+                            choose_gemarot=gemarot,
+                            chosen_by=person,
+                            available=False
+                        )
+            else:
+                guest_name = request.POST.get('guest_name')
+                guest_email = request.POST.get('guest_email')
+                guest, created = Guest.objects.get_or_create(email=guest_email, defaults={'name': guest_name})
+
+                # Ajouter les nouvelles réservations cochées
+                for gemara_id in gemara_ids:
+                    gemarot = get_object_or_404(Gemarot, pk=gemara_id)
+                    if not Gemara.objects.filter(session=session, choose_gemarot=gemarot, chosen_by_guest=guest).exists():
+                        Gemara.objects.create(
+                            session=session,
+                            choose_gemarot=gemarot,
+                            chosen_by_guest=guest,
+                            available=False
+                        )
 
         return redirect('session_detail', slug=slug)
