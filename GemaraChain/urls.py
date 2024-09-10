3f856ca120ea3397be223b336ccbd8ccfbd59af8
Nathan_Sroussi <77548288+NathanSr08@users.noreply.github.com>
2024-09-10 14:48:25 +0200

    Update urls.py

--- GemaraChain/urls.py
+++ GemaraChain/urls.py
@@ -22,7 +22,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', HomeView.as_view(), name='home'),
     path('create-session/', CreateSessionView.as_view(), name='create_session'),
-    path('session/<slug:slug>/', SessionDetailView.as_view(), name='session_detail'),  # Modification ici
+    path('session/<slug:slug>/', SessionDetailView.as_view(), name='session_detail'),
     path('login/', LoginView.as_view(), name='login'),
     path('signup/', SignupView.as_view(), name='signup'),
     path('logout/', LogoutView.as_view(), name='logout'),
