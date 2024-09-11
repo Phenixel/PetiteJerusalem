0ad401d89f1db77e528ae4fdd2ae241b5fb5628c
Nathan_Sroussi <77548288+NathanSr08@users.noreply.github.com>
2024-09-11 22:42:16 +0200

    (feat)  add user profile page with created and reserved sessions overview

--- GemaraChain/urls.py
+++ GemaraChain/urls.py
@@ -10,4 +10,5 @@ urlpatterns = [
     path('login/', LoginView.as_view(), name='login'),
     path('signup/', SignupView.as_view(), name='signup'),
     path('logout/', LogoutView.as_view(), name='logout'),
+    path('profile/', ProfileView.as_view(), name='profile'),
 ]
