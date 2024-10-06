dbd5ddc5ebadd17bab4ff6207e051b04005ce53c
Phenixel <yonathancardoso@outlook.fr>
2024-10-06 23:51:34 +0200

    (feat) #sucess ajout d'annonces sur le site web

--- static/js/script.js
+++ static/js/script.js
@@ -28,3 +28,37 @@ function updateThemeIcon(theme) {
         icon.classList.replace('fa-sun', 'fa-moon');
     }
 }
+
+function setCookie(name, value, days) {
+    const d = new Date();
+    d.setTime(d.getTime() + (days * 24 * 60 * 60 * 1000));
+    const expires = "expires=" + d.toUTCString();
+    document.cookie = name + "=" + value + ";" + expires + ";path=/";
+}
+
+function getCookie(name) {
+    const cname = name + "=";
+    const decodedCookie = decodeURIComponent(document.cookie);
+    const ca = decodedCookie.split(';');
+    for (let i = 0; i < ca.length; i++) {
+        let c = ca[i];
+        while (c.charAt(0) === ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(cname) === 0) {
+            return c.substring(cname.length, c.length);
+        }
+    }
+    return "";
+}
+
+function dismissAnnouncement() {
+    document.getElementById('announcement-banner').style.display = 'none';
+    setCookie('dismissed_announcement', 'true', 7); // Remember for 7 days
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    if (getCookie('dismissed_announcement') === 'true') {
+        document.getElementById('announcement-banner').style.display = 'none';
+    }
+});
