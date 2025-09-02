1f7e8790a72fc6b1ad5e4fa6c82659d85809f69d
Phenixel <yonathancardoso@outlook.fr>
2025-09-02 17:53:43 +0200

    feat: Ajout de l'initialisation de Google Analytics dans le fichier Firebase

--- firebase.ts
+++ firebase.ts
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app'
 import { GoogleAuthProvider } from 'firebase/auth'
+import { getAnalytics } from 'firebase/analytics'
 import { getFirestore } from 'firebase/firestore'
 
 // TODO: Add SDKs for Firebase products that you want to use
@@ -26,3 +27,6 @@ export const app = initializeApp(firebaseConfig)
 
 // Initialize Firestore
 export const db = getFirestore(app)
+
+// Initialize Analytics
+export const analytics = getAnalytics(app)
