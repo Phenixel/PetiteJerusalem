2dd598f1b1241364ad5b7f3270198941f2da11e2
Phenixel <yonathancardoso@outlook.fr>
2026-02-26 00:23:35 +0100

    refactor: Migrate i18n locale files from JSON to TypeScript format and update related configurations.

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,7 +1,7 @@
 import { createI18n } from "vue-i18n";
-import fr from "./locales/fr.json";
-import en from "./locales/en.json";
-import he from "./locales/he.json";
+import fr from "./locales/fr";
+import en from "./locales/en";
+import he from "./locales/he";
 
 export type SupportedLocale = "fr" | "en" | "he";
 
